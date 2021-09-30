7c3721e4541c1da2387580301715d33dc85fef8b
Akerele, Tosin James <akerele.james1699@gmail.com>
2021-09-30 05:50:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 const searchBtn = document.getElementById("submit");
 const search = document.getElementById("myInput");
-console.log(search.value);
+const price = document.getElementById("price-body");
 
+let data = [];
+let prices = [];
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  data.push(search.value);
+  console.log(data);
+
+  randomPrice();
+  updateDOM(randomPrice);
+});
+
+const updateDOM = (func) => {
+  price.innerText = `$${prices[1]}`;
+};
+
+const randomPrice = () => {
+  for (i = 0; i < 3; i++) {
+    let price = Math.floor(Math.random() * 100000);
+    prices.push(price);
+  }
+  console.log(prices);
+};
